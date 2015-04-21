72d24ef0ab0eb198de0658200e4c34cbbafa58be
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-04-21 23:09:24 +0100

    Fourth test failing

--- FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.fs
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.fs
@@ -4,7 +4,7 @@ open NUnit.Framework
 
 let fizzBuzz input = 
     if input = 1 then "1" 
-    elif input =2 then "2"
+    elif input = 2 then "2"
     else "Fizz"
 
 [<Test>]
@@ -18,3 +18,7 @@ let ``the number 2 is fizzBuzzed as is`` () =
 [<Test>]
 let ``the number 3 is fizzBuzzed as Fizz`` () =
     Assert.That ("Fizz" = fizzBuzz 3)
+
+[<Test>]
+let ``the number 4 is returned as is`` () =
+    Assert.That ("4" = fizzBuzz 4)
