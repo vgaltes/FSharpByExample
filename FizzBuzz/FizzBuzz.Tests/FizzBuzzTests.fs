3daee6068e6b3771be60aaa18e9ed00fa83f03c9
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-04-21 23:03:15 +0100

    Second test failing

--- FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.fs
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.fs
@@ -7,3 +7,7 @@ let fizzBuzz input = "1"
 [<Test>]
 let ``the number 1 is returned as is`` () =
     Assert.That ("1" = fizzBuzz 1)
+
+[<Test>]
+let ``the number 2 is returned as is`` () =
+    Assert.That ("2" = fizzBuzz 2)
