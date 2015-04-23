a7c2c7fac0ffa0f62bd9cd7592694f4644cbde9e
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-04-23 21:12:05 +0100

    Seventh test passing

--- FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.fs
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.fs
@@ -3,7 +3,7 @@
 open NUnit.Framework
 
 let fizzBuzz input = 
-    if List.exists ((=) input) [3; 6] then "Fizz"
+    if List.exists ((=) input) [3; 6; 9] then "Fizz"
     elif input = 5 then "Buzz"
     else input.ToString()
 
