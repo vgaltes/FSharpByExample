280bb791fa509e27ca07a8d7cba99b7898442733
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-05-03 16:28:17 +0100

    Withdraw

--- BankKata/BankKata/BankKata.fs
+++ BankKata/BankKata/BankKata.fs
@@ -1,5 +1,9 @@
 ﻿module BankKata
     module Prod =
+        type Result<'T> =
+            | Success of 'T
+            | Failure of string
+
         type Currency = 
         |USD
 
@@ -9,6 +13,9 @@
 
         let deposit amount currency {Amount = (oldAmount, _)} =
             {Amount = (oldAmount + amount, USD)}
+
+        let withdraw amount currency account =
+            Success {Amount = (30, USD)}
 
     module Tests =
         open Prod
@@ -28,3 +35,8 @@
         let ``money deposited into a bank account should be added to current amount``() =
             let bankAccount = newAccount |> deposit 10 USD |> deposit 5 USD
             Assert.AreEqual((15, USD), bankAccount.Amount)
+
+        [<Test>]
+        let ``money can be withdrawn from a bank account``() =
+            let (Success(bankAccount)) = newAccount |> deposit 50 USD |> withdraw 20 USD
+            Assert.AreEqual({ Amount = (30, USD) }, bankAccount)
