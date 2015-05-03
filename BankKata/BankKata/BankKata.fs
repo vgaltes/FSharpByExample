b4080a0d763a1e0be16e807ba788bb696e64be47
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-05-03 15:55:39 +0100

    Project creation

---
+++ BankKata/BankKata/BankKata.fs
@@ -0,0 +1,11 @@
+﻿module BankKata
+    module Prod =
+        let bank = ()
+
+    module Tests =
+        
+        open NUnit.Framework
+
+        [<Test>]
+        let ``test framework``() = 
+            Assert.IsTrue(true)
